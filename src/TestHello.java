5a508d8b2de2370951c93e22cfda6c1cc3cfb0e7
duminchao <du_minchao@126.com>
2016-03-18 21:56:54 +0800

    Update TestHello.java
    remote update

--- src/TestHello.java
+++ src/TestHello.java
@@ -5,7 +5,8 @@ public class TestHello {
 		System.out.println("11111111111");
 		System.out.println("22222222222");
 		System.out.println("33333333333");
-		this is the service update;
+		//this is the service update;
+		System.out.println("44remote update44");
 	}
 
 }
